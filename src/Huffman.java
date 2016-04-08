e376c9287df9f2cfd6896b78437c28b0b9d25bb5
Thomas Nix <tnix2@uncc.edu>
2016-04-08 00:00:49 -0400

    Read in file via user input of file name, calculated the frequency of each letter in the file (line by line) and printed the results.

---
+++ src/Huffman.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Huffman {
+	
+	HashMap<String, Integer> dictionary = new HashMap<String, Integer>();
+	String currentLine = "";
+
+	
+	public void findFreq(String inLine){
+		System.out.println(inLine);
+		int indexCounter=0;
+		
+		for (int i = 0; i < inLine.length(); i++){
+			String currentChar=String.valueOf(inLine.charAt(indexCounter));
+			if (!dictionary.keySet().contains(currentChar)){
+				dictionary.put(currentChar, 1);
+			}else{
+				dictionary.replace(currentChar, dictionary.get(currentChar)+1);
+			}
+			indexCounter++;
+		}
+	}
+	
+	public void printhMap(){
+		System.out.println("\n\nLETTER FREQUENCIES:\n===============\n");
+		for (String keys:dictionary.keySet()){
+			System.out.println("Letter: " + keys + "\tFreq: " + dictionary.get(keys));
+		}
+	}
+}
--- src/driver.java
+++ src/driver.java
@@ -1,7 +1,23 @@
+import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.*;
 
 public class driver {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws FileNotFoundException{
+		Scanner userInput = new Scanner (System.in);
+		System.out.println("Enter a file name for a text file, do NOT include .txt at the end!");
+		String userResponse = userInput.nextLine();
 		
+		File inFile = new File(userResponse.concat(".txt"));
+		Scanner readFile = new Scanner(inFile);
+		
+		Huffman readIn = new Huffman();
+		System.out.println("\nLines read:\n");
+		while (readFile.hasNextLine()){
+			readIn.findFreq(readFile.nextLine());
+		}
+		
+		readIn.printhMap();
 	}
 }
